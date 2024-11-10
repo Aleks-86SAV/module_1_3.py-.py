ae9c049b4699eb5e0ecc12f74046655a10949610
Alexander Smirnov <sanyablask@gmail.com>
2024-11-10 19:00:23 +0300

    Add files via upload

---
+++ 'module_1_3.py'.py
@@ -0,0 +1,15 @@
+name = "Modul"
+print(name,type(name))
+name = 15
+print(name,type(name))
+name = 17
+print(name,type(name))
+name = "Alexander"
+print(name,type(name))
+age = 38
+print(age,type(age))
+new_age = age+1
+print(new_age,type(new_age))
+Is_Student = True
+print(Is_Student,type(Is_Student))
+
